ce86c8a2d5f3b8f7a84da83e592918f760d4e8ad
Vivek Goel <vivekgoel15@gmail.com>
2020-06-19 14:31:58 +0530

    added routing and authentication

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 // app.js
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var product = require('./routes/product'); // Imports routes for the products
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const productRoutes = require('./routes/product'); // Imports routes for the products
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+const dotenv = require('dotenv');
+dotenv.config();
+
 var app = express();
-
-
 // Set up mongoose connection
 var mongoose = require('mongoose');
-//var mongoDB = 'mongodb://127.0.0.1:27017/test';
-var mongoDB = 'mongodb://test:test@cluster0-shard-00-00-pl5om.mongodb.net:27017,cluster0-shard-00-01-pl5om.mongodb.net:27017,cluster0-shard-00-02-pl5om.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+var mongoDB = 'mongodb://127.0.0.1:27017/test';
+//var mongoDB = 'mongodb://test:test@cluster0-shard-00-00-pl5om.mongodb.net:27017,cluster0-shard-00-01-pl5om.mongodb.net:27017,cluster0-shard-00-02-pl5om.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI || mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
@@ -20,12 +26,23 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(expressValidator());
 app.use(cors());
-app.use('/products', product);
 
-var port = 1234;
+app.use('/', authRoutes);
+app.use('/', userRoutes);
+app.use('/', productRoutes);
+app.use(function(err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ error: 'Unauthorized!' });
+    }
+});
+
+var port = process.env.PORT || 1234;
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
--- routes/product.js
+++ routes/product.js
@@ -1,23 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const { createProductValidator } = require('../validator');
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 var product_controller = require('../controllers/product');
 
-
 // a simple test url to check that all of our files are communicating correctly.
-router.get('/test', product_controller.test);
+router.get('/products/test', product_controller.test);
 
+router.post('/products/create', createProductValidator, product_controller.product_create);
 
-router.post('/create', product_controller.product_create);
+router.get('/products/:id', product_controller.product_details);
 
-router.get('/:id', product_controller.product_details);
+router.get('/products', product_controller.products);
 
-router.get('/', product_controller.products);
+router.put('/products/:id/update', createProductValidator, product_controller.product_update);
 
-router.put('/:id/update', product_controller.product_update);
-
-router.delete('/:id/delete', product_controller.product_delete);
-
+router.delete('/products/:id/delete', product_controller.product_delete);
 
 module.exports = router;
